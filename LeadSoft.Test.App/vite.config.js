d8e8e48cce533bc97de189f839963ccd89251081
Willian Beck Ribeiro <willian1561@outlook.com>
2025-02-18 04:33:25 -0300

    fixed front, dockerfile's front and docker compose

--- LeadSoft.Test.App/vite.config.js
+++ LeadSoft.Test.App/vite.config.js
@@ -5,7 +5,14 @@ import plugin from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [tailwindcss(), plugin()],
+    preview: {
+        port: 4173,
+        strictPort: true,
+    },
     server: {
         port: 64461,
-    }
+        strictPort: true,
+        host: true,
+        origin: "http://0.0.0.0:64461",
+    },
 })
